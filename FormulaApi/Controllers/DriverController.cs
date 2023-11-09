c4e48398547f02da210337b736e8b56cf9de3515
artyom <artyom.aghekyan2006@gmail.com>
2023-11-09 22:32:30 +0400

    Add project files.

---
+++ FormulaApi/Controllers/DriverController.cs
@@ -0,0 +1,105 @@
+﻿using FormulaApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace FormulaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private static List<Driver> _drivers = new List<Driver>()
+        {
+            new Driver()
+            {
+                Id = 1,
+                Name = "Test1",
+                DeriverNumber = "TestNum1",
+                Team = "TestTeam1"
+            },
+            new Driver()
+            {
+                Id = 2,
+                Name = "Test2",
+                DeriverNumber = "TestNum2",
+                Team = "TestTeam2"
+            },
+            new Driver()
+            {
+                Id = 3,
+                Name = "Test3",
+                DeriverNumber = "TestNum3",
+                Team = "TestTeam3"
+            }
+        };
+
+        [ProducesResponseType(200)]
+        [HttpGet]
+        public IActionResult GetDriversAll()
+        {
+            var drivers = _drivers.ToList();
+            return Ok(drivers);
+        }
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [HttpGet("{id}")]
+        public IActionResult GetDriverById(int id)
+        {
+            var driver = _drivers.FirstOrDefault(x => x.Id == id);
+            if (driver == null)
+            {
+                return NotFound();
+            }
+            return Ok(driver);
+        }
+        [ProducesResponseType(201)]
+        [HttpPost]
+        public IActionResult PostDriver(Driver driver) {
+            var newDriver = new Driver
+            {
+                Id = _drivers.ToList().Last().Id + 1,
+                Name = driver.Name,
+                DeriverNumber = driver.DeriverNumber,
+                Team = driver.Team
+
+            };
+            _drivers.Add(newDriver);
+            return Ok(newDriver);
+        }
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [HttpDelete("{id}")]
+        public IActionResult DeleteDriver(int id)
+        {
+            var driver = _drivers.FirstOrDefault(x => x.Id == id);
+            if (driver is null)
+            {
+                return BadRequest();
+            }
+            _drivers.Remove(driver);
+            return Ok("Driver was deleted succesfully");
+
+        }
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [HttpPut]
+        public IActionResult UpdateDriver(int id, Driver request)
+        {
+            var driver = _drivers.FirstOrDefault(x => x.Id == id);
+
+            if (driver is null) return NotFound();
+
+            driver.Name = request.Name;
+            driver.DeriverNumber = request.DeriverNumber;
+            driver.Team = request.Team;
+
+            return Ok(driver);
+
+
+        }
+
+    } 
+
+}
+
---
+++ FormulaApi/Data/DriverDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FormulaApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FormulaApi.Data
+{
+    public class DriverDbContext : DbContext
+    {
+        public DriverDbContext(DbContextOptions<DriverDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
