4c59a653741fc2ec97fbbee804b73bcc8754af83
artyom <artyom.aghekyan2006@gmail.com>
2023-11-09 22:42:34 +0400

    Integrate database connectivity into the application

--- FormulaApi/Program.cs
+++ FormulaApi/Program.cs
@@ -1,7 +1,11 @@
+using FormulaApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DriverDbContext>(options
+    => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
